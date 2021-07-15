79ea28c9202b93717722cf1128453d011a23a1a8
MayaraSs <may_stein@hotmail.com>
2021-07-14 22:57:27 -0300

    add api

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import router from "./routes";
+
+dotenv.config();
+
+
+const PORT: number = 4000;
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
---
+++ src/routes.ts
@@ -0,0 +1,77 @@
+
+import express, {Request, Response} from 'express'
+import { v4 as uuidv4 } from 'uuid';
+
+export const router = express.Router()
+
+interface Establishment {
+  name: string
+  locale: string
+  cnpj: string
+  id: string
+}
+
+const establishments: Array<Establishment> = [];
+
+router.post("/login", async (req: Request, res: Response) => {
+  try {
+    console.log(req.body)
+    res.status(200).send({
+      'name': 'Mayara',
+      'email': 'stein.mayara@gmail.com',
+      'token': 'mock-token'
+    });
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+
+
+router.get("/establishments", async (req: Request, res: Response) => {
+  try {
+    console.log(establishments)
+    res.status(200).send(establishments);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+router.post("/establishment", async (req: Request, res: Response) => {
+  try {
+    console.log(req.body)
+
+    const establishment = {
+      name: req.body.name,
+      cnpj: req.body.cnpj,
+      locale: req.body.locale,
+    }
+
+    establishments.push({id: uuidv4(),...establishment})
+
+    res.status(201);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+router.delete("/establishment/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+
+    establishments.map((e, i) => {
+      if(e.id === id) {
+        console.log(i)
+        establishments.splice(i, 1);
+
+        // delete establishments[i];
+      }
+    }) 
+    res.status(201);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+
+export default router;
